9c99aff7d3611c2cb2b59b4cd8327a9465405c89
Shrek617 <108195444+Shrek617@users.noreply.github.com>
2022-07-05 21:17:59 +0200

    Meet, DiscordGuess Remastered!
    1. Prevent ban (adding success list) because two friend request sent = ban.
    2. Added number to start without waiting for number when already checked.
    3. Changed recommend delay from 10 second to 15 second.
    4. And small fixes.

--- discordguess.py
+++ discordguess.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import logging
 from time import sleep
 from colorama import init, Fore
 
@@ -10,14 +11,17 @@ print(Fore.LIGHTGREEN_EX + """
    / __ \\(_)_____________  _________/ / ____/_  _____  __________
   / / / / / ___/ ___/ __ \\/ ___/ __  / / __/ / / / _ \\/ ___/ ___/
  / /_/ / (__  ) /__/ /_/ / /  / /_/ / /_/ / /_/ /  __(__  |__  ) 
-/_____/_/____/\\___/\\____/_/   \\__,_/\\____/\\__,_/\\___/____/____/  
+/_____/_/____/\\___/\\____/_/   \\__,_/\\____/\\__,_/\\___/____/____/  Remastered  
                                                                  """ + Fore.RESET);
 
 # Discord has a 10 requests/minute rate limit on the Add Friend function.
-# Rate limiting delays are implemented, but use a 10 second delay to avoid suspicion.
+# Rate limiting delays are implemented, but use a 15 second delay to avoid suspicion.
 
 token = input('Token: ')      # Discord Authorization Token
-delay = int(input('Delay: ')) # Seconds to wait between requests
+delay = int(input('Delay [Rec. 15]: ')) # Seconds to wait between requests
+number = int(input('Number: ')) # Number to check of tags
+totalnumber = number + 1
+print(Fore.LIGHTGREEN_EX + str(number) + '+' + '1' + '=' + str(totalnumber) + ' ' + '(Total Number)' + Fore.RESET)
 req = {}
 req['username'] = input('Username: ') # Username to guess
 token = token.replace('"', '')
@@ -38,38 +42,64 @@ s.get('https://discordapp.com/channels/@me')
 print('Cookies -->', s.cookies.get_dict())
 print()
 
+# Logging setup
+formatter = logging.Formatter('%(message)s') 
+def setup_logger(name, log_file, level=logging.INFO):
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+# Logging
+logger = setup_logger('log', 'log.txt')
+logger.info('New session started')
+super_logger = setup_logger(req['username'], req['username'] + '.txt')
+
 # Start bruteforce
 found = False
-i = 0
-while i<10000:
+i = number                              
+while i<1000:
     normalSleep = True
     i += 1
     print('[',i,'] ', sep = '', end = '')
     req['discriminator'] = i
-    r = s.post('https://discordapp.com/api/v6/users/@me/relationships', data = json.dumps(req), headers = headers)
-    if r.status_code == 204:   # Friend Request sent
-        print(Fore.LIGHTGREEN_EX + 'Success!' + Fore.RESET)
-        found = True
-        break
-    elif r.status_code == 400: # Incorrect Discriminator
-        print('Incorrect')
-    elif r.status_code == 429: # Rate Limit
-        i -= 1
-        p = (json.loads(r.text)['retry_after'])/1000
-        print(Fore.MAGENTA + 'Rate limit: retrying after', p, 'seconds.' + Fore.RESET)
-        normalSleep = False
-        sleep(p)
-    elif r.status_code == 401: # Invalid token
-        print(Fore.LIGHTRED_EX + 'Invalid Token!')
-        break
-    else:
-        print('Unknown error', r.status_code,'-->',r.text)
+    check = str(i)
+    with open(req['username'] + '.txt') as f:
+        match = check in f.read().splitlines()
+    if match == True: # Prevent Ban | Two friend request sent = ban
+        print(Fore.LIGHTGREEN_EX + 'Ignoring because this Discord Tag in success list.' + Fore.RESET)
+        print('Discord Tag: ', Fore.LIGHTBLUE_EX + req['username'], '#', i, sep = '' + Fore.RESET)
+    if match != True:
+        r = s.post('https://discordapp.com/api/v6/users/@me/relationships', data = json.dumps(req), headers = headers)
+        if r.status_code == 204:   # Friend Request sent
+            print(Fore.LIGHTGREEN_EX + 'Success! Now im ignoring this Discord Tag to prevent ban.' + Fore.RESET)
+            logging.info('[', i, ']', 'Success!')
+            print('Discord Tag: ', Fore.LIGHTBLUE_EX + req['username'], '#', i, sep = '' + Fore.RESET)
+            super_logging.info('[', i, ']', 'Success! Now im ignoring this Discord Tag to prevent ban. !!! DONT DELETE OR REDACTING THIS FILE TO PREVENT BAN !!!')
+            found = True
+        elif r.status_code == 400: # Incorrect Discriminator
+            print('Incorrect')
+            print(Fore.LIGHTRED_EX + 'This Discord Tag doesn\'t exist.' + Fore.RESET)
+            logging.info('[', i, ']', 'Incorrect')
+        elif r.status_code == 429: # Rate Limit
+            i -= 1
+            p = (json.loads(r.text)['retry_after'])/1000
+            print(Fore.MAGENTA + 'Rate limit: retrying after', p, 'seconds.' + Fore.RESET)
+            logging.warning('[', i, ']', 'Rate limit: retrying after', p, 'seconds.')
+            normalSleep = False
+            sleep(p)
+        elif r.status_code == 401: # Invalid token
+            print(Fore.LIGHTRED_EX + 'Invalid Token!')
+            logging.info('[', i, ']', 'Invalid Token!')
+            break
+        else:
+            print('Unknown error', r.status_code,'-->',r.text)
+            logging.info('[', i, ']', 'Unknown error')
     if normalSleep:
         sleep(delay)
 
-print()
-if found:
-    print('Discord Tag: ', Fore.LIGHTBLUE_EX + req['username'], '#', i, sep = '')
-else:
-    print(Fore.LIGHTRED_EX + 'This Discord Tag doesn\'t exist.')
 print()
